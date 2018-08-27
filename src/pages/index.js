e02856c973f094adc7ff3744fe0e4d2a4ea1aabb
Nestor Benito Fernandez <nestorbenito@nestorbdezsimac.attlocal.net>
2018-08-27 14:48:35 -0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi FreeCodeCamp!</h1>
+    <p>Looking forward to join the FreeCodeCamp Hackathon in SF.</p>
+    <p>Let's build something!</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage

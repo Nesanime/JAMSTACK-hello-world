29a55999ddbc930509ff002c8d91adf2259904ed
nesanime <nesanime@gmail.com>
2018-08-27 14:58:24 -0700

    second  commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ const IndexPage = () => (
     <h1>Hi FreeCodeCamp!</h1>
     <p>Looking forward to join the FreeCodeCamp Hackathon in SF.</p>
     <p>Let's build something!</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/">Go to page 3</Link>
   </div>
 )
 
